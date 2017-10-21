f22fe74c0b1e19bdbff416d5f5ae56e0ab3baa74
Nafay <manafay@github.com>
2017-10-21 18:23:53 +0530

    Added supporting tests for map and filter

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,29 @@
+import { filter, map } from './index';
+
 test('jest', () => {
   expect(true).toBeTruthy();
+});
+
+test('map with array of numbers', () => {
+  const arr: number[] = [1,2,3,4,5];
+  const mapFn = (x: number) => x;
+  expect(map(arr, mapFn)).toEqual(arr);
+});
+
+test('map with array of strings', () => {
+  const arr: string[] = ["H","HE","HEL","HELL","HELLO"];
+  const mapFn = (x: string) => x.length;
+  expect(map(arr, mapFn)).toEqual([1,2,3,4,5]);
+});
+
+test('filter for even numbers', () => {
+  const arr: number[] = [1,2,3,4,5];
+  const predicate = (x: number) => x % 2 === 0;
+  expect(filter(arr, predicate)).toEqual([2,4]);
+});
+
+test('filter for string length', () => {
+  const arr: string[] = ["H","HE","HEL","HELL","HELLO"];
+  const predicate = (x: string) => x.length >= 3;
+  expect(filter(arr, predicate)).toEqual(["HEL","HELL","HELLO"]);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 console.log('hello world');
 
 
-function map<T1, T2>(source: T1[], mapFn: <U>(arg: U) => T2 ): T2[] {
+export function map<T1, T2>(source: T1[], mapFn: <U>(arg: U) => T2 ): T2[] {
   const result: T2[] = [];
   source.forEach((val: T1) => result.push(mapFn(val)));
   return result;
 }
 
-function filter<T1>(source: T1[], pred: <T2>(arg: T2) => boolean): T1[] {
+export function filter<T1>(source: T1[], pred: <T2>(arg: T2) => boolean): T1[] {
   const result: T1[] = [];
   source.forEach((val: T1) => {
     if (pred(val)) {
