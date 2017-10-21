2600a857438546bc21238a6c8f5f3f5945c7a2aa
Nafay <manafay@github.com>
2017-10-21 17:30:27 +0530

    Map & Filter generic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 console.log('hello world');
+
+
+function map<T1, T2>(source: T1[], mapFn: <U>(arg: U) => T2 ): T2[] {
+  const result: T2[] = [];
+  source.forEach((val: T1) => result.push(mapFn(val)));
+  return result;
+}
+
+function filter<T1>(source: T1[], pred: <T2>(arg: T2) => boolean): T1[] {
+  const result: T1[] = [];
+  source.forEach((val: T1) => {
+    if (pred(val)) {
+      result.push(val);
+    } 
+  });
+  return result;
+}
